daed0ec322f1d3b97a9c931abad15106e410a105
Davi Silva <davisilvaphoto@gmail.com>
2022-07-27 11:31:16 -0300

    feat: add other methods into Cart

--- module-01/src/lib/Cart.js
+++ module-01/src/lib/Cart.js
@@ -1,6 +1,8 @@
 import Dinero from 'dinero.js';
 import { find, remove } from 'lodash';
 
+import { calculateDiscount } from './utils';
+
 const Money = Dinero;
 
 Money.defaultCurrency = 'BRL';
@@ -24,19 +26,26 @@ export class Cart {
   }
 
   getTotal() {
-    return this.items.reduce(
-      (acc, item) =>
-        acc.add(Money({ amount: item.quantity * item.product.price })),
-      Money({ amount: 0 }),
-    );
+    return this.items.reduce((acc, { quantity, product, condition }) => {
+      const amount = Money({ amount: quantity * product.price });
+      let discount = Money({ amount: 0 });
+
+      if (condition) {
+        discount = calculateDiscount(amount, quantity, condition);
+      }
+
+      return acc.add(amount).subtract(discount);
+    }, Money({ amount: 0 }));
   }
 
   summary() {
-    const total = this.getTotal().getAmount();
+    const total = this.getTotal();
+    const formatted = total.toFormat('$0,0.00');
     const items = this.items;
 
     return {
       total,
+      formatted,
       items,
     };
   }
@@ -47,7 +56,7 @@ export class Cart {
     this.items = [];
 
     return {
-      total,
+      total: total.getAmount(),
       items,
     };
   }
