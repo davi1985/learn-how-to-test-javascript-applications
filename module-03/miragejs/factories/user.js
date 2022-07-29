1bcf4f33a50230fa16c85149f88781d074281c91
Davi Silva <davisilvaphoto@gmail.com>
2022-07-29 15:38:04 -0300

    fix: adjust miragejs

--- module-03/miragejs/factories/user.js
+++ module-03/miragejs/factories/user.js
@@ -1,13 +1,7 @@
 import { randomNumber } from './utils';
 
-/*
- * Mirage JS guide on Factories: https://miragejs.com/docs/data-layer/factories
- */
 import { Factory } from 'miragejs';
 
-/*
- * Faker Github repository: https://github.com/Marak/Faker.js#readme
- */
 import faker from 'faker';
 
 export default {
--- module-03/miragejs/fixtures/index.js
+++ module-03/miragejs/fixtures/index.js
@@ -1,7 +1,3 @@
-/*
- * Mirage JS guide on Fixtures: https://miragejs.com/docs/data-layer/fixtures
- */
-
 import products from './products.json';
 
 export default { products };
