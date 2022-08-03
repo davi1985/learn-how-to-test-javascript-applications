1f3073b1289ca78ac3e1bbb804d4e428fc92dc9b
Davi Silva <davisilvaphoto@gmail.com>
2022-08-03 09:02:41 -0300

    feat: add useCartStore

--- module-03/store/cart/index.js
+++ module-03/store/cart/index.js
@@ -1,10 +1,35 @@
 import create from 'zustand';
 
+const initialState = {
+  open: false,
+  products: [],
+};
+
+const addProduct = (store, product) => {
+  if (store.state.products.includes(product)) {
+    return store.state.products;
+  }
+
+  return [...store.state.products, product];
+};
+
 export const useCartStore = create((set) => ({
   state: {
-    open: false,
+    ...initialState,
   },
   actions: {
-    toggle: () => set((store) => ({ state: { open: !store.state.open } })),
+    toggle: () =>
+      set((store) => ({
+        state: { ...store.state, open: !store.state.open },
+      })),
+
+    reset: () => set((store) => ({ state: { ...initialState } })),
+
+    add: (product) =>
+      set((store) => {
+        return {
+          state: { open: true, products: addProduct(store, product) },
+        };
+      }),
   },
 }));
